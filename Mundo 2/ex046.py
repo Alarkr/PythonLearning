b8048bf14ebdac2ab75530b03103809bc96ca4e0
Paulo Alarcão <alarcaorpaulo@gmail.com>
2022-09-22 15:56:25 -0300

    Adicionando ex046

--- Mundo 2/ex046.py
+++ Mundo 2/ex046.py
@@ -1,2 +1,11 @@
-print('vai pra casa do caralho')
-print('teste de caralho a quatro')
+from time import sleep
+print('-'*18)
+print('Fogos de Artificio')
+print('-'*18)
+
+for i in range(10, -1, -1):
+    print(i)
+    sleep(0.5)
+print('=-'*18)
+print('EEEEEEEEH, FELIZ ANO NOVOOOOO!!!!!!!')
+print('=-'*18)
